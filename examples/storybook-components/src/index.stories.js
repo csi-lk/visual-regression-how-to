c67490e62f17654c7864f19b45aeead56b59606f
Callum Silcock <github@csi.lk>
2020-06-07 17:02:07 +0100

    style(storybook-components): changed button border

--- examples/storybook-components/src/index.stories.js
+++ examples/storybook-components/src/index.stories.js
@@ -5,5 +5,6 @@ export const withText = () => '<button class="btn">Hello World</button>'
 export const withEmoji = () => {
   const button = document.createElement('button')
   button.innerText = '😀 😎 👍 💯'
+  button.style.cssText = 'border: 1px solid black;'
   return button
 }
