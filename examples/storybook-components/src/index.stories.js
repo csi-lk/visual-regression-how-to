6cf811b3d03b88ee15a78ec10cda89f1fd7c7664
Callum Silcock <github@csi.lk>
2020-06-07 18:25:12 +0100

    feat(storybook-components): add new input component and change button styling
    button border now green

--- examples/storybook-components/src/index.stories.js
+++ examples/storybook-components/src/index.stories.js
@@ -5,6 +5,14 @@ export const withText = () => '<button class="btn">Hello World</button>'
 export const withEmoji = () => {
   const button = document.createElement('button')
   button.innerText = '😀 😎 👍 💯'
-  button.style.cssText = 'border: 1px solid black;'
+  button.style.cssText = 'border: 1px solid green;'
   return button
+}
+
+export const newThing = () => {
+  const input = document.createElement('input')
+  input.type = 'text'
+  input.placeholder = 'I am an input...'
+  input.style.cssText = 'border: 1px solid grey;'
+  return input
 }
